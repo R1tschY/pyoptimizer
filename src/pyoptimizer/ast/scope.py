e225fe7a96bad58bf758e534eb4d314686615fbd
R1tschY <r1tschy@posteo.de>
2020-06-27 15:19:37 +0200

    Extended scope logic

--- src/pyoptimizer/ast/scope.py
+++ src/pyoptimizer/ast/scope.py
@@ -1,6 +1,9 @@
 # -*- coding=utf-8 -*-
 import ast
-from ast import AST, Global, Module, Name, Nonlocal, Store
+from ast import AST, AsyncFunctionDef, ClassDef, FunctionDef, Global, ListComp, \
+    Module, Name, \
+    Nonlocal, \
+    Store
 from dataclasses import dataclass, field
 from typing import Any, List, Optional
 
@@ -9,8 +12,8 @@ from typing import Any, List, Optional
 class Scope:
     local: bool
     names: List[str] = field(default_factory=list)
-    global_: List[str] = field(default_factory=list)
-    nonlocal_: List[str] = field(default_factory=list)
+    globals_: List[str] = field(default_factory=list)
+    nonlocals_: List[str] = field(default_factory=list)
     item: Optional[AST] = None
     parent: Optional["Scope"] = None # TODO
 
@@ -19,8 +22,8 @@ class Scope:
         return Scope(local=False, item=item)
 
     @classmethod
-    def new_local(cls, item: Optional[AST] = None):
-        return Scope(local=True, item=item)
+    def new_local(cls, item: Optional[AST] = None, locals: List[str] = None):
+        return Scope(local=True, item=item, names=locals or [])
 
 
 class ScopingVisitor(ast.NodeVisitor):
@@ -30,32 +33,54 @@ class ScopingVisitor(ast.NodeVisitor):
         self.current_scope = Scope.new_global()
 
     def visit_Name(self, node: Name) -> Any:
+        node._pyo_scope = self.current_scope
+
         if isinstance(node.ctx, Store):
-            self.current_scope.names.append(node.id)
+            if node.id not in self.current_scope.globals_ and \
+                    node.id not in self.current_scope.nonlocals_:
+                if node.id not in self.current_scope.names:
+                    self.current_scope.names.append(node.id)
+                node._pyo_is_local = True
+                return
+
+        node._pyo_is_local = node.id in self.current_scope.names
 
     def visit_Global(self, node: Global) -> Any:
-        self.current_scope.global_.extend(node.names)
+        self.current_scope.globals_.extend(node.names)
 
     def visit_Nonlocal(self, node: Nonlocal) -> Any:
-        self.current_scope.nonlocal_.extend(node.names)
+        self.current_scope.nonlocals_.extend(node.names)
 
-    def visit_Module(self, node: Module) -> Any:
+    def visit_FunctionDef(self, node: FunctionDef) -> Any:
+        self.current_scope.names.append(node.name)
+
+        node_args = node.args
+        args = [arg.arg for arg in node_args.args]
+        if node_args.vararg:
+            args.append(node_args.vararg.arg)
+        args.extend([arg.arg for arg in node_args.kwonlyargs])
+        if node_args.kwarg:
+            args.append(node_args.kwarg.arg)
+
+        self.visit_scope(node, args)
+
+    def visit_AsyncFunctionDef(self, node: AsyncFunctionDef) -> Any:
+        self.current_scope.names.append(node.name)
+        self.visit_scope(node)
+
+    def visit_ClassDef(self, node: ClassDef) -> Any:
+        self.current_scope.names.append(node.name)
+        self.visit_scope(node)
+
+    def visit_scope(self, node: AST, locals: List[str] = None):
         outer_scope = self.current_scope
-        self.current_scope = Scope.new_global(node)
+        self.current_scope = Scope.new_local(node, locals)
         node._pyo_scope = self.current_scope
         self.generic_visit(node)
         self.current_scope = outer_scope
 
-    def visit_FunctionDef(self, node: Module) -> Any:
-        outer_scope = self.current_scope
-        self.current_scope = Scope.new_local(node)
-        node._pyo_scope = self.current_scope
-        self.generic_visit(node)
-        self.current_scope = outer_scope
-
-    def visit_ClassDef(self, node: Module) -> Any:
-        outer_scope = self.current_scope
-        self.current_scope = Scope.new_local(node)
-        node._pyo_scope = self.current_scope
-        self.generic_visit(node)
-        self.current_scope = outer_scope
+    visit_Module = visit_scope
+    visit_ListComp = visit_scope
+    visit_DictComp = visit_scope
+    visit_SetComp = visit_scope
+    visit_GeneratorExp = visit_scope
--- tests/test_scoping.py
+++ tests/test_scoping.py
@@ -1,35 +1,174 @@
 # -*- coding=utf-8 -*-
 import ast
 from textwrap import dedent
+from types import CodeType, FunctionType
+from typing import Dict, List, Tuple
+
+import pytest
 
 from pyoptimizer.ast.scope import Scope, ScopingVisitor
 
+testdata = [
+    pytest.param(
+        """
+            def fn():
+                a = 1
+        """,
+        id="func scope"
+    ),
+    pytest.param(
+        """
+            def fn():
+                a = 1
+                
+            b = 1
+        """,
+        id="func_scope_reset"
+    ),
+    pytest.param(
+        """
+            def fn():
+                a = 1
+                a = 1
+                a = 1
+        """,
+        id="func no dups"
+    ),
 
-def test_func_scope():
-    code = """
-        def fn():
-            a = 1
-    """
+    pytest.param(
+        """
+            def fn(a, b, c):
+                pass
+        """,
+        id="func args"
+    ),
+    pytest.param(
+        """
+            def fn(a, *args, **kwargs):
+                pass
+        """,
+        id="func *args"
+    ),
+    pytest.param(
+        """
+            def fn(abc=None, **kwargs):
+                pass
+        """,
+        id="func kwargs"
+    ),
 
-    tree: ast.Module = ast.parse(dedent(code))
+    pytest.param(
+        """
+            def fn():
+                for i in range(10000):
+                    pass
+        """,
+        id="func_scope_for"
+    ),
+    pytest.param(
+        """
+            def fn():
+                for i in range(10000):
+                    for j in range(10000):
+                        pass
+        """,
+        id="func_scope_for_for"
+    ),
+
+    # Comp
+    pytest.param(
+        """
+            def fn():
+                return [i for i in range(10000)]
+        """,
+        id="func listcomp"
+    ),
+    pytest.param(
+        """
+            def fn():
+                return {i: None for i in range(10000)}
+        """,
+        id="func dictcomp"
+    ),
+    pytest.param(
+        """
+            def fn():
+                return {i for i in range(10000)}
+        """,
+        id="func setcomp"
+    ),
+    pytest.param(
+        """
+            def fn():
+                return (i for i in range(10000))
+        """,
+        id="func generator"
+    ),
+
+    # Closures
+    pytest.param(
+        """
+            def fn():
+                return lambda x: x
+        """,
+        id="func lambda"
+    ),
+    pytest.param(
+        """
+            def fn():
+                def closure():
+                    x = 1
+        """,
+        id="func closure"
+    ),
+    pytest.param(
+        """
+            def fn():
+                class A:
+                    x = 1
+        """,
+        id="func closure"
+    ),
+
+    # global/nonlocal
+    pytest.param(
+        """
+            def fn():
+                global x, y
+                x = 1
+        """,
+        id="func global"
+    ),
+    # pytest.param(
+    #     """
+    #         x = 1
+    #
+    #         def fn():
+    #             nonlocal x
+    #             x = 1
+    #     """,
+    #     id="func nonlocal"
+    # ),
+]
+
+
+@pytest.mark.parametrize("code", testdata)
+def test_func_scope(code):
+    code = dedent(code)
+    tree: ast.Module = ast.parse(code)
 
     ScopingVisitor().visit(tree)
 
-    assert isinstance(tree.body[0]._pyo_scope, Scope)
-    assert tree.body[0]._pyo_scope.names == ["a"]
+    _, locals_ = scoped_exec(code)
+    assert tuple(tree.body[0]._pyo_scope.names) == local_vars(locals_["fn"])
 
 
-def test_func_scope_reset():
-    code = """
-        def fn():
-            a = 1
-            
-        b = 1
-    """
+def scoped_exec(code: str) -> Tuple[Dict, Dict]:
+    globals_ = {}
+    locals_ = {}
+    exec(code, globals_, locals_)
+    return globals_, locals_
 
-    tree: ast.Module = ast.parse(dedent(code))
 
-    ScopingVisitor().visit(tree)
-
-    assert tree.body[0]._pyo_scope.names == ["a"]
-    assert tree._pyo_scope.names == ["b"]
+def local_vars(fn: FunctionType) -> Tuple[str]:
+    return fn.__code__.co_varnames
